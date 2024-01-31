e0c05adf2ff39b8636de78fb6dbf29472aac9b78
MrKeviscool <danielandkev@outlook.com>
2024-01-31 14:18:29 +1030

    new file:   .vscode/settings.json 	modified:   blueballz.cpp 	new file:   compile.sh

--- blueballz.cpp
+++ blueballz.cpp
@@ -1,4 +1,33 @@
 #include <SFML/Graphics.hpp>
 #define ballsize 30
-
+#define fps 0.008333333
+float xspeed = 0, yspeed = 0, xpos = 30, ypos = 540, rotaionspd = 0;
 sf::CircleShape ball(ballsize);
+void calculaterotation(), display(), logic();
+int main(){
+    /////SETUP/////////
+    sf::RenderWindow window(sf::VideoMode(1920, 1080), "Blue Ballz - MrKeviscool", sf::Style::Fullscreen);
+    ///////END SETUP//////////
+        while(window.isOpen()){ 
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if(event.type == sf::Event::Closed){
+                window.close();
+            }
+        }
+        ////////LOOP//////////
+        display(window);
+        //////END LOOP////////
+    }
+}
+
+void calculaterotation(){
+
+}
+
+void display(sf::RenderWindow &window){
+    window.clear(sf::Color::Black);
+    window.draw(ball);
+    display();
+}
---
+++ compile.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+g++  blueballz.cpp -o blubal -lsfml-graphics -lsfml-window -lsfml-system
+#g++ bouncingball.o -o bb -lsfml-graphics -lsfml-window -lsfml-system
+#rm ./bouncingball.o
+./bluebal
