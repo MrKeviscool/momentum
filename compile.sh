e382bf13e93256eb6801e280aaad0c8f3cf9b85f
MrKeviscool <danielandkev@outlook.com>
2024-02-02 20:07:59 +1030

    modified:   .gitignore 	modified:   compile.sh     fixed name lmao

--- compile.sh
+++ compile.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-g++  blueballz.cpp -o blubal -lsfml-graphics -lsfml-window -lsfml-system
-#g++ bouncingball.o -o bb -lsfml-graphics -lsfml-window -lsfml-system
-#rm ./bouncingball.o
+g++  momentum.cpp -o game -lsfml-graphics -lsfml-window -lsfml-system
 #./blubal
